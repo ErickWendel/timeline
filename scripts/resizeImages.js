44d5cdf6925d7c0fe40db2c59f84190b656c1060
erickwendel <erick.workspace@gmail.com>
2019-05-18 20:19:11 -0400

    resizingImages
    Signed-off-by: erickwendel <erick.workspace@gmail.com>

--- scripts/resizeImages.js
+++ scripts/resizeImages.js
@@ -10,24 +10,30 @@ const [mkdirAsync, existsAsync, readAsync] = [
 
 const Jimp = require('jimp');
 const folder_600x600 = `600x600`;
-// const myPath = '../2015-06-12-webscraping_com_c#';
+const folder_1024x1024 = `1024x1024`;
 
-async function reScale(myPath) {
-  if (!(await existsAsync(`${myPath}/${folder_600x600}`)))
-    await mkdirAsync(`${myPath}/${folder_600x600}`);
+const sizes = [folder_1024x1024, folder_600x600];
+
+async function reScale(myPath, size) {
+  const finalFolder = `${myPath}/${size}`;
+
   try {
+    if (!(await existsAsync(`${finalFolder}`)))
+      await mkdirAsync(`${finalFolder}`);
+
     const files = await readAsync(myPath);
     const items = files.filter(
-      item => item !== '.DS_Store' && item !== folder_600x600,
+      item => item !== '.DS_Store' && !~sizes.indexOf(size),
     );
 
-    const outPut = `${myPath}/${folder_600x600}`;
+    const outPut = `${finalFolder}`;
 
     for (file of items) {
       if (file === '.DS_Store') continue;
-      console.log('reading', file);
-
-      const lenna = await Jimp.read(`${myPath}/${file}`);
+      const pathFile = `${myPath}/${file}`;
+      console.log('file', pathFile);
+      console.log('creating', `${outPut}/${file}`);
+      const lenna = await Jimp.read(pathFile);
       lenna
         .scaleToFit(600, 600)
         .quality(60)
@@ -35,6 +41,7 @@ async function reScale(myPath) {
     }
   } catch (err) {
     console.log('error', err);
+    throw err;
   }
 }
 
@@ -43,8 +50,10 @@ async function main() {
     .filter(({ photos }) => !!photos)
     .map(({ title, date, photos }) => ({ title, date, photos }));
 
-  const t = items.map(({ photos }) => reScale(`../${photos}`));
-  const final = await Promise.all(t);
-  console.log('final', final);
+  const t = items.map(({ photos }) => console.log('photos', photos));
+  //   const t = items.map(({ photos }) => reScale(`../${photos}`, folder_600x600));
+  //   const t = items.map(({ photos }) => reScale(`../${photos}`, folder_1024x1024));
+  return Promise.all(t);
+  //   console.log('final', final);
 }
 main();
